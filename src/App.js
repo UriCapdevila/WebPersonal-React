58f29a78ce07f89123b5375a6da854501c6bdeb2
uricapdevil4 <uricapdevil4@gmail.com>
2025-10-29 17:38:35 +0000

    update 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [bongs, setBongs] = useState(0);
+  const [consentGiven, setConsentGiven] = useState(false);
+
+  const handleGrantConsent = () => {
+    setConsentGiven(true);
+  };
+
+  const handleBong = () => {
+    setBongs(bongs + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="consentement" hidden={consentGiven}>
+        <p>Ce site nécessite une autorisation pour fonctionner.</p>
+        <button id="demander" onClick={handleGrantConsent}>Demander l'autorisation</button>
+      </div>
+      <h1>Bong !</h1>
+      <p>Nombre de bongs : <span id="bongs">{bongs}</span></p>
+      <button id="bong" onClick={handleBong}>Bong !</button>
+      <p>Made with ❤️ by <a href="https://www.google.com">the Google team</a></p>
     </div>
   );
 }
